a9827ee6d174ccdac6922c3d579a538c24a3a2cb
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-07-10 20:56:03 -0300

    Cansei de mexer na velocidade diagonal

---
+++ GOOSE/Assets/Prog/Inputschema.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "InputSchema")]
+public class Inputschema : ScriptableObject
+{
+    public string horizontal_axis;
+    public string vertical_axis;
+
+    public KeyCode[] play;
+    public KeyCode[] back;
+
+
+    public bool GetKey(KeyCode[] keys)
+    {
+        foreach (var key in keys)
+        {
+            if (Input.GetKey(key)) return true;
+        }
+        return false;
+    }
+}
---
+++ GOOSE/Assets/Prog/Mov.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+
+public class Mov : MonoBehaviour
+{
+
+    public Inputschema input;
+    public float vel;
+    private Rigidbody2D rb;
+    private Vector2 moveVec;
+
+    private float moveLimiter = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveVec.x = Input.GetAxis(input.horizontal_axis) ;
+        moveVec.y = Input.GetAxis(input.vertical_axis) ;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (moveVec.x != 0 && moveVec.y != 0) 
+        {
+           
+            moveVec.x *= moveLimiter;
+            moveVec.y *= moveLimiter;
+        }
+
+        rb.velocity = moveVec * vel;
+
+        Debug.Log(rb.velocity);
+    }
+}
